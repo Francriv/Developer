c6c07185a8083db5cce03c52634ca7033c769c72
Cesar Rivas <csrivas24@gmail.com>
2018-04-29 16:41:56 -0500

    todo chido hasta aqui

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* El menu deslizable esta aqui  */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+/* El menu deslizable termina aqui  */
